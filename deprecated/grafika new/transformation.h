b0aceefff2400aa660ccd7ab4d6af0fd6b544675
Adin Baskoro <13513058@std.stei.itb.ac.id>
2016-02-26 15:40:45 +0700

    wow

--- deprecated/grafika new/transformation.h
+++
@@ -1,3 +0,0 @@
-void translateList(int distanceX, int distanceY, int list[], int length);
-int* rotateList(int degree, int centerX, int centerY, int *list, int length);
-void scaleList(int scale, int centerX, int centerY, int list[], int length);
