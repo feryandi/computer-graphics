cd4fa108a206c951816b9ae200e9d429a63508ee
adarwawan <adarwawan@gmail.com>
2016-03-20 12:44:13 +0700

    gedung gedung itb

---
+++ GIS/gedung-gedung.cpp
@@ -0,0 +1,91 @@
+#include "ITB"
+
+Kelompok gedung-gedung
+
+Gerbang Depan & Aula
+- Gapura_Kiri
+- Gapura_Kanan
+- Aula Barat
+- Aula Timur
+
+Zona Barat Daya
+- Gedung Fisika
+- Gedung Sipil
+- BSC A
+
+Zona Tenggara
+- Seni Rupa (9)
+- 9009 LFM (8)
+- Planalogi (12)
+- Teknik Lingkungan (13)
+- Geodesi (11)
+- Arsitektur (10)
+
+Zona CC
+- CC Barat
+- CC Timur
+- Lapangan Basket
+- Lapangan Cinta
+
+Zona 4 Labtek
+- Labtek V
+- Labtek VI
+- Labtek VII
+- Labtek VIII
+- Intel
+
+Zona GKU Barat
+- Lab Radar
+- Lab Konversi Energi
+- GKU Barat
+
+Zona GKU Timur
+- GKU Timur (15)
+- FTSL RIL (Yang ada kolamnya) (Atasnya 15)
+- Lab Doping
+- FSRD Seni Murni (14)
+
+Zona Timur Jauh
+- Kimia (31)
+- BSC B (32)
+- FTTM (33)
+- Antah Berantah (34,35)
+
+Zona Sunken
+- Sunken
+- PAU
+- Perpustakaan
+- CAS
+- CADL / Gedung Serbaguna
+
+Zona Gedung Kembar
+- Oktagon
+- TVST
+- PLN
+- Comlabs / Laboratorium Fisika Dasar
+- Mektan
+- Bengkel Bawahnya Mektan
+
+Zona Labtek Biru
+- Laboratorium Aeronotika
+- Labtek Biru
+- Gedung Metalurgi
+
+Zona Barat Laut
+- Gedung Matematika
+- SBM
+
+Zona Saraga Sabuga
+- Saraga
+- Sabuga
+
+Zona Salman
+- Masjid Salman
+- Kompleks Salman
+
+Zona Luar
+- IRO
+- LPIK
+- SBM S2
+
+- 
