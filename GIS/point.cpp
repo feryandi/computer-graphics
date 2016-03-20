f41126636214b84c9e794252492a188d73825d4f
adarwawan <adarwawan@gmail.com>
2016-03-20 19:02:21 +0700

    cobain dong

--- GIS/point.cpp
+++ GIS/point.cpp
@@ -45,13 +45,14 @@ void Point::moveZ(int _z){
   z += _z;
 }
 
-void Point::rotate(float degree, int center_x, int center_y){
+void Point::rotate(float degree, int center_x, int center_y, int posX, int posY ){
       double val = PI/180.0;
-      int newX, newY;
+      int absX = x + posX;
+      int absY = y + posY;
       // std::cout<<"before: "<<"x= "<<x<<" y= "<<y<<std::endl;
       newX = ( cos(degree * val) * (x-center_x) - sin(degree * val) * (y-center_y) + center_x );
       newY = ( sin(degree * val) * (x-center_x) + cos(degree * val) * (y-center_y) + center_y );
       // std::cout<<"after: "<<"x= "<<x<<" y= "<<y<<std::endl;
-      x = newX;
-      y = newY;
+      x = absX - posX;
+      y = absY - posY;
 }
